34f53fd5308d1884e85643909cd2154d4b5a8b24
haozifan <haozifan@DESKTOP-DH9F1MI>
2017-03-22 11:50:29 +0800

    删除多余的命名空间 修改a标签样式

--- src/NewBlogger/Controllers/HomeController.cs
+++ src/NewBlogger/Controllers/HomeController.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.IO;
-using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using NewBlogger.Application.Interface;
 
