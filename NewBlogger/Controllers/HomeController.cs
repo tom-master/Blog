f4a278e254984327e246d9f8ebf5ff0d62ebff02
haoxiaofan <569192072@qq.com>
2017-05-21 13:35:53 +0800

    修改代码结构

--- NewBlogger/Controllers/HomeController.cs
+++ NewBlogger/Controllers/HomeController.cs
@@ -28,157 +28,167 @@ namespace NewBlogger.Controllers
             _hostingEnvironment = hostingEnvironment;
         }
 
-        #region pages
+        #region 页面
 
-        /// <summary>
-        /// 博客首页
-        /// </summary>
-        /// <returns></returns>
-        [HttpGet]
-        public IActionResult Index()
-        {
-            ViewBag.Title = "初代目黑影";
-
-            return View();
-        }
-
-        /// <summary>
-        /// 博客详情页
-        /// </summary>
-        /// <param name="blogId"></param>
-        /// <returns></returns>
-        [HttpGet]
-        public IActionResult BlogDetail(Guid blogId = default(Guid))
-        {
-
-            if (blogId == Guid.Empty)
+            /// <summary>
+            /// 博客首页
+            /// </summary>
+            /// <returns></returns>
+            [HttpGet]
+            public IActionResult Index()
             {
-                throw new ArgumentNullException($"{nameof(blogId)}");
+                ViewBag.Title = "初代目黑影";
+
+                return View();
             }
 
-            _blogService.AddViewCount(blogId);
+            /// <summary>
+            /// 博客详情页
+            /// </summary>
+            /// <param name="blogId"></param>
+            /// <returns></returns>
+            [HttpGet]
+            public IActionResult BlogDetail(Guid blogId = default(Guid))
+            {
 
-            var blog = _blogService.GetBlog(blogId);
+                if (blogId == Guid.Empty)
+                {
+                    throw new ArgumentNullException($"{nameof(blogId)}");
+                }
 
-            ViewBag.Title = blog.Title;
+                _blogService.AddViewCount(blogId);
 
-            return View(blog);
-        }
+                var blog = _blogService.GetBlog(blogId);
+
+                ViewBag.Title = blog.Title;
+
+                return View(blog);
+            }
+
 
         #endregion
 
-        /// <summary>
-        /// 获取所有分类
-        /// </summary>
-        /// <returns></returns>
-        [HttpGet]
-        public IActionResult GetCategories()
-        {
-            return Json(new
+        #region 获取
+            /// <summary>
+            /// 获取所有分类
+            /// </summary>
+            /// <returns></returns>
+            [HttpGet]
+            public IActionResult GetCategories()
             {
-                categories = _categoryService.GetCategorys()
-            });
-        }
-
-
-        /// <summary>
-        /// 获取所有文章
-        /// </summary>
-        /// <returns></returns>
-        [HttpGet]
-        public IActionResult GetBlogs(Int32 pageIndex, Int32 pageSize, Guid categoryId = default(Guid))
-        {
-            Int32 totalCount;
-
-            var blogs = _blogService.GetBlogs(categoryId, pageIndex, pageSize, out totalCount);
-
-            return Json(new
-            {
-                totalCount,
-                blogs
-            });
-        }
-
-        /// <summary>
-        /// 回复当前博客
-        /// </summary>
-        /// <returns></returns>
-        [HttpPost]
-        public IActionResult Reply()
-        {
-
-            if (String.IsNullOrEmpty(Request.Form["name"]))
-            {
-                throw new ArgumentNullException("name");
+                return Json(new
+                {
+                    categories = _categoryService.GetCategorys()
+                });
             }
 
-            if (String.IsNullOrEmpty(Request.Form["email"]))
+
+            /// <summary>
+            /// 获取所有文章
+            /// </summary>
+            /// <returns></returns>
+            [HttpGet]
+            public IActionResult GetBlogs(Int32 pageIndex, Int32 pageSize, Guid categoryId = default(Guid))
             {
-                throw new ArgumentNullException("email");
+                Int32 totalCount;
+
+                var blogs = _blogService.GetBlogs(categoryId, pageIndex, pageSize, out totalCount);
+
+                return Json(new
+                {
+                    totalCount,
+                    blogs
+                });
+            }
+        #endregion
+
+        #region 回复
+
+            /// <summary>
+            /// 回复当前博客
+            /// </summary>
+            /// <returns></returns>
+            [HttpPost]
+            public IActionResult Reply()
+            {
+
+                if (String.IsNullOrEmpty(Request.Form["name"]))
+                {
+                    throw new ArgumentNullException("name");
+                }
+
+                if (String.IsNullOrEmpty(Request.Form["email"]))
+                {
+                    throw new ArgumentNullException("email");
+                }
+
+                if (String.IsNullOrEmpty(Request.Form["blogId"]))
+                {
+                    throw new ArgumentNullException("blogId");
+                }
+
+                if (Guid.Parse(Request.Form["blogId"]) == Guid.Empty)
+                {
+                    throw new ArgumentNullException("blogId");
+                }
+
+                if (String.IsNullOrEmpty(Request.Form["message"]))
+                {
+                    throw new ArgumentNullException("message");
+                }
+
+                var nickName = Request.Form["name"];
+
+                var email = Request.Form["email"];
+
+                var blogId = Guid.Parse(Request.Form["blogId"]);
+
+                var replyId = (Request.Form["replyId"] + "").Length <= 0 ? Guid.Empty : Guid.Parse(Request.Form["replyId"]);
+
+                var message = Request.Form["message"];
+
+                _commentService.AddComment(nickName, email, blogId, message, replyId);
+
+                return Json(new { status = 1 });
             }
 
-            if (String.IsNullOrEmpty(Request.Form["blogId"]))
+        #endregion
+
+        #region 上传图片
+            /// <summary>
+            /// 文章回复上传图片
+            /// </summary>
+            /// <returns></returns>
+            [HttpPost]
+            public IActionResult UploadImage()
             {
-                throw new ArgumentNullException("blogId");
+
+                var file = Request.Form.Files[0];
+
+                if (file==null)
+                {
+                    throw new ArgumentNullException("file");
+                }
+
+                var filePath = $@"{_hostingEnvironment.WebRootPath}\UploadImage\";
+
+                if (!Directory.Exists(filePath))
+                {
+                    Directory.CreateDirectory(filePath);
+                }
+
+                var fullPath = $@"{filePath}{file.FileName}";
+
+                using (var fileStream = new FileStream(fullPath, FileMode.Create, FileAccess.Write))
+                {
+                    file.CopyTo(fileStream);
+
+                    fileStream.Flush();
+                }
+
+                return Content(fullPath.Replace(_hostingEnvironment.WebRootPath, $"http://{Request.Host.Value}").Replace("\\", "/"));
             }
 
-            if (Guid.Parse(Request.Form["blogId"]) == Guid.Empty)
-            {
-                throw new ArgumentNullException("blogId");
-            }
-
-            if (String.IsNullOrEmpty(Request.Form["message"]))
-            {
-                throw new ArgumentNullException("message");
-            }
-
-            var nickName = Request.Form["name"];
-
-            var email = Request.Form["email"];
-
-            var blogId = Guid.Parse(Request.Form["blogId"]);
-
-            var replyId = (Request.Form["replyId"] + "").Length <= 0 ? Guid.Empty : Guid.Parse(Request.Form["replyId"]);
-
-            var message = Request.Form["message"];
-
-            _commentService.AddComment(nickName, email, blogId, message, replyId);
-
-            return Json(new { status = 1 });
-        }
-
-        /// <summary>
-        /// 文章回复上传图片
-        /// </summary>
-        /// <returns></returns>
-        [HttpPost]
-        public IActionResult UploadImage()
-        {
-
-            var file = Request.Form.Files[0];
-
-            if (file==null)
-            {
-                throw new ArgumentNullException("file");
-            }
-
-            var filePath = $@"{_hostingEnvironment.WebRootPath}\UploadImage\";
-
-            if (!Directory.Exists(filePath))
-            {
-                Directory.CreateDirectory(filePath);
-            }
-
-            var fullPath = $@"{filePath}{file.FileName}";
-
-            using (var fileStream = new FileStream(fullPath, FileMode.Create, FileAccess.Write))
-            {
-                file.CopyTo(fileStream);
-
-                fileStream.Flush();
-            }
-
-            return Content(fullPath.Replace(_hostingEnvironment.WebRootPath, $"http://{Request.Host.Value}").Replace("\\", "/"));
-        }
+        #endregion
     }
 }
